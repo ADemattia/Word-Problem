52b63ce04b394c3c08de6f58df55818e61b14692
ADemattia <127196391+ADemattia@users.noreply.github.com>
2025-01-05 17:54:18 +0100

    Update OwA_lab.scala

--- OwA_lab.scala
+++ OwA_lab.scala
@@ -75,15 +75,16 @@ object Word_Problem extends lisa.Main {
   import lisa.prooflib.ProofTacticLib.ProofTactic
   import lisa.prooflib.Library
 
-  object OwA extends ProofTactic {
+  object Backward_Proof extends ProofTactic {
     def solve(using lib: library.type, proof: lib.Proof)(goal: Sequent): proof.ProofTacticJudgement = {
       if goal.right.size != 1 
-       then proof.InvalidProofTactic("OwA can only be applied to solve goals of the form (s1 <= t1, s2 <= t2, ..., sn <= tn) |- s <= t")
+       then proof.InvalidProofTactic("Backward_Proof can only be applied to solve goals of the form (s1 <= t1, s2 <= t2, ..., sn <= tn) |- s <= t")
       else { // Starting the proof of goal
 
         def extractRelevantTerm(goal: Sequent): Set[Word_Problem.Term] = {
           val formula_set = goal.left ++ goal.right
           var relevant_term: Set[Word_Problem.Term] = Set()
+
           for (elem <- formula_set) {
             val elem_af = elem.asInstanceOf[AtomicFormula]
             val left = elem_af.args.head
@@ -99,6 +100,7 @@ object Word_Problem extends lisa.Main {
                 output_set = set + a
                 return extract(output_set, a)
               }
+
             if elem.label == n then {
               val a = elem.args.head
               val b = elem.args.tail.head
@@ -106,6 +108,7 @@ object Word_Problem extends lisa.Main {
               output_set = output_set + b
               return (extract(output_set, a) ++ extract(output_set, b))
               }
+
             if elem.label == u then {
               val a = elem.args.head
               val b = elem.args.tail.head
@@ -113,6 +116,7 @@ object Word_Problem extends lisa.Main {
               output_set = output_set + b
               return (extract(output_set, a) ++ extract(output_set, b))
               }
+
               return output_set
           }
 
@@ -137,10 +141,10 @@ object Word_Problem extends lisa.Main {
           return addComplementedTerm(known_term)
         }
 
+        var flag_left_trans = 0 
+
+
         val relevant_term = extractRelevantTerm(goal)
-        var i = 0
-        val debug = 0
-        var ncall = 0
         val axiom_set = goal.left
         var axiom_term: Set[Term] = Set()
         var proven_sequent: Set[proof.ValidProofTactic] = Set()
@@ -153,23 +157,18 @@ object Word_Problem extends lisa.Main {
         }
 
         def prove(goal: Sequent): proof.ProofTacticJudgement = {
-          ncall = ncall + 1
           //Check in proven_sequent
           for (elem <- proven_sequent) {
             if elem.bot == goal then {
               val s1 = have(goal) by Tautology.from(have(elem))
+              flag_left_trans = 0
               return s1.judgement
             }
           }
           
           //Check in visited_sequent
           if visited_sequent.contains(goal) then {
-            if debug == 1 then {
-              println(s"Yet Visited ${i}")
-              println(goal.right.head)
-              i += 1
-              println("-------------------")
-            }
+            flag_left_trans = 0
             return proof.InvalidProofTactic("The inequality is not true in general")
           } else {
             visited_sequent = visited_sequent + goal
@@ -178,80 +177,50 @@ object Word_Problem extends lisa.Main {
             val left = formula.args.head
             val right = formula.args.tail.head
 
+            // 0-premises checks
             // Reflexivity
             if left == right then {
-              if debug == 1 then {
-                println(s"Reflexivity ${i}")
-                i += 1
-                println(goal.right.head)
-                println("-------------------")
-              }
               val s1 = have(goal) by Tautology.from(reflexivity of (x := left))
               proven_sequent = proven_sequent + s1.judgement.asInstanceOf[proof.ValidProofTactic]
+              flag_left_trans = 0
               return s1.judgement
             }
 
             
             // Axiom P7 : x <= ne(ne(x))
             if right == Word_Problem.ne(Word_Problem.ne(left)) then {
-              if debug == 1 then {
-                println(s"Axiom P7 ${i}")
-                i += 1
-                println(goal.right.head)
-                println("-------------------")
-              }
               val s1 = have(goal) by Tautology.from(P7 of (x := left))
               proven_sequent = proven_sequent + s1.judgement.asInstanceOf[proof.ValidProofTactic]
+              flag_left_trans = 0
               return s1.judgement
             }
 
             // Axiom P7': ne(ne(x)) <= x
             if left == Word_Problem.ne(Word_Problem.ne(right)) then {
-              if debug == 1 then {
-                println(s"Axiom P7p ${i}")
-                i += 1
-                println(goal.right.head)
-                println("-------------------")
-              }
               val s1 = have(goal) by Tautology.from(P7p of (x := right))
               proven_sequent = proven_sequent + s1.judgement.asInstanceOf[proof.ValidProofTactic]
+              flag_left_trans = 0
               return s1.judgement
             }
 
             // Axiom P3 - least element : 0 <= x 
             if left == zero then {
-              if debug == 1 then {
-                println(s"zero <= x ${i}")
-                i += 1
-                println(goal.right.head)
-                println("-------------------")
-              }
               val s1 = have(goal) by Tautology.from(P3 of (x := right))
               proven_sequent = proven_sequent + s1.judgement.asInstanceOf[proof.ValidProofTactic]
+              flag_left_trans = 0
               return s1.judgement
             }
 
             // Axiom P3' - least element : x <= 1
             if right == one then {
-              if debug == 1 then {
-                println(s"x <= one ${i}")
-                i += 1
-                println(goal.right.head)
-                println("-------------------")
-              }
               val s1 = have(goal) by Tautology.from(P3p of (x := left))
               proven_sequent = proven_sequent + s1.judgement.asInstanceOf[proof.ValidProofTactic]
+              flag_left_trans = 0
               return s1.judgement
             }
 
             // P8
             if left.label == Word_Problem.ne && right.label == Word_Problem.ne then {
-              if debug == 1 then {
-                println(s"ne(y) <= ne(x) ${i}")
-                i += 1
-                println(goal.right.head)
-                println("-------------------")
-              }
               val a = left.args.head
               val b = right.args.head
               var new_goal_1 = Sequent(goal.left, Set(b <= a))
@@ -259,17 +228,12 @@ object Word_Problem extends lisa.Main {
               if s1.isValid then {
                 val s3 = have(goal) by Tautology.from(have(s1), P8 of (x := b, y := a))
                 proven_sequent = proven_sequent + s3.judgement.asInstanceOf[proof.ValidProofTactic]
+                flag_left_trans = 0
                 return s3.judgement
               }
             }
 
             if left.label == n then {
-              if debug == 1 then {
-                println(s"left n ${i}")
-                i += 1
-                println(goal.right.head)
-                println("-------------------")
-              }
               val a = left.args.head
               val b = left.args.tail.head
               var new_goal_1 = Sequent(goal.left, Set(a <= right))
@@ -277,6 +241,7 @@ object Word_Problem extends lisa.Main {
               if s1.isValid then {
                 val s3 = have(goal) by Tautology.from(have(s1), P4 of (x := a, y := b), transitivity of (x := left, y := a, z := right))
                 proven_sequent = proven_sequent + s3.judgement.asInstanceOf[proof.ValidProofTactic]
+                flag_left_trans = 0
                 return s3.judgement
               }
               var new_goal_2 = Sequent(goal.left, Set(b <= right))
@@ -284,17 +249,13 @@ object Word_Problem extends lisa.Main {
               if s2.isValid then {
                 val s3 = have(goal) by Tautology.from(have(s2), P5 of (x := a, y := b), transitivity of (x := left, y := b, z := right))
                 proven_sequent = proven_sequent + s3.judgement.asInstanceOf[proof.ValidProofTactic]
+                flag_left_trans = 0
                 return s3.judgement
               }
             }
-
+            // Axiom P6' : x <= z & y <= z => x u y <= z 
+            if flag_left_trans == 0 then {
             if left.label == u then {
-              if debug == 1 then {
-                println(s"left u ${i}")
-                i += 1
-                println(goal.right.head)
-                println("-------------------")
-              }
               val a = left.args.head
               val b = left.args.tail.head
               var new_goal_1 = Sequent(goal.left, Set(a <= right))
@@ -309,14 +270,11 @@ object Word_Problem extends lisa.Main {
                 }
               }
             }
+          } else {
+            flag_left_trans = 0
+          }
 
             if right.label == n then {
-              if debug == 1 then {
-                println(s"Right n ${i}")
-                i += 1
-                println(goal.right.head)
-                println("-------------------")
-              }
               val a = right.args.head
               val b = right.args.tail.head
               var new_goal_1 = Sequent(goal.left, Set(left <= a))
@@ -327,18 +285,13 @@ object Word_Problem extends lisa.Main {
                 if s2.isValid then {
                   val s3 = have(goal) by Tautology.from(have(s1), have(s2), P6 of (x := left, y := a, z := b))
                   proven_sequent = proven_sequent + s3.judgement.asInstanceOf[proof.ValidProofTactic]
+                  flag_left_trans = 0
                   return s3.judgement
                 }
               }
             }
 
             if right.label == u then {
-              if debug == 1 then {
-                println(s"Right u ${i}")
-                i += 1
-                println(goal.right.head)
-                println("-------------------")
-              }
               val a = right.args.head
               val b = right.args.tail.head
               var new_goal_1 = Sequent(goal.left, Set(left <= a))
@@ -346,6 +299,7 @@ object Word_Problem extends lisa.Main {
               if s1.isValid then {
                 val s3 = have(goal) by Tautology.from(have(s1), P4p of (x := a, y := b), transitivity of (x := left, y := a, z := right))
                 proven_sequent = proven_sequent + s3.judgement.asInstanceOf[proof.ValidProofTactic]
+                flag_left_trans = 0
                 return s3.judgement
               }
               var new_goal_2 = Sequent(goal.left, Set(left <= b))
@@ -353,6 +307,7 @@ object Word_Problem extends lisa.Main {
               if s2.isValid then {
                 val s3 = have(goal) by Tautology.from(have(s2), P5p of (x := a, y := b), transitivity of (x := left, y := b, z := right))
                 proven_sequent = proven_sequent + s3.judgement.asInstanceOf[proof.ValidProofTactic]
+                flag_left_trans = 0
                 return s3.judgement
               }
             }
@@ -363,26 +318,22 @@ object Word_Problem extends lisa.Main {
             if s1.isValid then {
               val s3 = have(goal) by Tautology.from(NC of (x := left, y := right), have(s1))
               proven_sequent = proven_sequent + s3.judgement.asInstanceOf[proof.ValidProofTactic]
+              flag_left_trans = 0
               return s3.judgement
             }
 
 
             val current_relevant_term = extractRelevantTerm(goal)
             val useful_relevant_term = current_relevant_term.intersect(relevant_term)
+
+            if flag_left_trans == 0 then {
             for (elem <- useful_relevant_term) {
               if !(left == elem) && !(right == elem) then {
-                if debug == 1 then {
-                  println(s"Transitivity with Known Term ${i}")
-                  i += 1
-                  println(goal.right.head)
-                  println("Cut Term")
-                  println(elem)
-                  println("-------------------")
-                }
                 var new_goal_1 = Sequent(goal.left, Set(elem <= right))
                 val s1 = prove(new_goal_1)
                 if s1.isValid then {
-                  var new_goal_2 = Sequent(goal.left, Set(left <= elem))
+                  val new_goal_2 = Sequent(goal.left, Set(left <= elem))
+                  flag_left_trans = 1 
                   val s2 = prove(new_goal_2)
                   if s2.isValid then {
                     val s3 = have(goal) by Tautology.from(have(s1), have(s2), transitivity of (x := left, y := elem, z := right))
@@ -392,17 +343,15 @@ object Word_Problem extends lisa.Main {
                 }
               }
             }
+          } else {
+            flag_left_trans = 0
+          }
+            flag_left_trans = 0
             return proof.InvalidProofTactic("The inequality is not true in general")
           }
         }
         goal.right.head match {
           case <=(left_t: Term, right_t: Term) => {
-            println(goal)
-            println("Number of Known Term")
-            println(relevant_term.size)
-            println("Number of call")
-            println(ncall)
-            println("-------------------")
             return prove(goal)
           }
 
@@ -411,22 +360,15 @@ object Word_Problem extends lisa.Main {
             var s1 = prove(new_goal_1)
             if s1.isValid then {
               var new_goal_2 = Sequent(goal.left, Set(right <= left))
-              println(new_goal_2)
               var s2 = prove(new_goal_2)
               if s2.isValid then {
                 val s3 = have(goal) by Tautology.from(have(s1), have(s2), antisymmetry of (x := left, y := right))
-                println(goal)
-                println("Number of Known Term")
-                println(relevant_term.size)
-                println("Number of call")
-                println(ncall)
-                println("-------------------")
                 return s3.judgement}
-                else {return return proof.InvalidProofTactic("Word Problem can only be applied to solve goals of form aaaaaaaa") }
-            } else {return proof.InvalidProofTactic("Word Problem can only be applied to solve goals of form bbbbbbb") }
+                else {return return proof.InvalidProofTactic("The equality is not true in general") }
+            } else {return proof.InvalidProofTactic("The inequality is not true in general") }
           }
 
-          case _ => return proof.InvalidProofTactic("Word Problem can only be applied to solve goals of form bla bla bla")
+          case _ => return proof.InvalidProofTactic("Backward_Proof can only be applied to solve goals of the form (s1 <= t1, s2 <= t2, ..., sn <= tn) |- s <= t")
         }
 
       }
@@ -435,57 +377,57 @@ object Word_Problem extends lisa.Main {
 
 
 
-
   val DeMorgan1 = Theorem((ne(x) n ne(y)) === ne(x u y)) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
 
   val DeMorgan2 = Theorem(ne(x n y)===(ne(x) u ne(y))) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
 
   val EXTOA = Theorem(one <= (x n (ne(x) u y)) |- one <= y) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
 
   val test1 = Theorem(one <= (ne(x) u x)) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
   val test2 = Theorem(x <= (x u y)) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
   val test3 = Theorem((y n x) <= x) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
   val test4 = Theorem((x n y) <= (y u z)) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
   val semiDistributivity = Theorem((x u (y n z)) <= ((x u y) n (x u z))) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
 
   val idempotence = Theorem((x u (y n (ne(z) u z))) === (y u (x n one))) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
 
   val double_false = Theorem((x n ne(x)) === (y n ne(y))) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
 
   val EXRM = Theorem(((x u z) <= y) |- x <= y) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
 
   val EXVE = Theorem((x <= ne(z), y <= ne(z)) |- ((x u y) n z) === zero) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
 
   val EXSG = Theorem(x <= y |- y === (x u y)) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
 
   val EXPI = Theorem((x n z) === (x n z n ne(y n ne(x)))) {
-    have(thesis) by OwA.solve
+    have(thesis) by Backward_Proof.solve
   }
 
 }
+
